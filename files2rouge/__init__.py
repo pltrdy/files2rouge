38f414e17cb58a67bc63dd3886d02344d69aa831
pltrdy <pltrdy@gmail.com>
2019-10-01 12:56:09 +0200

    bump version

--- files2rouge/__init__.py
+++ files2rouge/__init__.py
@@ -3,4 +3,5 @@ from files2rouge.files2rouge import run
 from files2rouge.files2rouge import main
 from files2rouge.settings import Settings
 
+__version__ = "2.1.0"
 __all__ = [absolute_import, run, main, Settings]
